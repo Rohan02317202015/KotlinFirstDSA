517ddbb893634ebfa3802b4b3ae0f19246d34458
Ronnie <rohangupta.ino@gmail.com>
2025-07-03 19:00:34 +0530

    Readme for maths and sorting algos updated

--- app/src/main/java/com/rohan/dsaprime/basics/recursion/code.kt
+++ app/src/main/java/com/rohan/dsaprime/basics/recursion/code.kt
@@ -1,5 +1,32 @@
 package com.rohan.dsaprime.basics.recursion
 
 fun main() {
-    println("This is my code")
+
+    println(Fibonacci().series(30,0,0))
+}
+
+class CheckPalindrome  {
+
+    fun isPalindrome(word: String, low: Int, high: Int): Boolean {
+        if( low >= high ) return true
+
+        if( word[low] != word[high]) return false
+
+       return isPalindrome(word, low + 1, high - 1)
+    }
+}
+
+class Fibonacci {
+
+    fun series(term: Int, acc: Int, previous: Int) {
+        if(term <= 0) return
+        if(acc == 0 && previous == 0){
+            print("0 1 ")
+            series(term - 2, 1, 1)
+        } else {
+            print("$acc ")
+            val updatedAcc = acc + previous
+            series(term - 1, updatedAcc, acc)
+        }
+    }
 }
